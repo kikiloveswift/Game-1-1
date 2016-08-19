cf663efa629dbab00d1ce64847053771eda083de
konglee <konglee873@gmail.com>
2016-08-19 16:39:05 +0800

    C++入门
    基础的C++

--- Game-1-1/main.cpp
+++ Game-1-1/main.cpp
@@ -8,8 +8,40 @@
 
 #include <iostream>
 
+//using namespace::std;
+
+//函数声明
+void func (void);
+static int count = 10;
+
 int main(int argc, const char * argv[]) {
+    
+    
     // insert code here...
     std::cout << "Hello, World!\n";
+//    cout << "fuck\n";
+//    wchar_t *wchar = "我是个帅比";
+//    cout << "size of wchar_t " << sizeof(wchar_t) << endl;
+//    cout << "size of char " << sizeof(char) << endl;
+//    const int d = 20;
+    int a = 10;
+    int b = 2;
+    int c = a + b;
+    std :: cout << c << std:: endl;
+    
+    while (count--) {
+        if (count == 5) {
+            func();
+        }
+    }
+    
     return 0;
+}
+
+void func (void){
+
+    static int i = 5;
+    i++;
+    std:: cout << "i = " << i <<  std:: endl;
+    
 }
